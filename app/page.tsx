cca4f062c017fa37c9c144588bfd0e047c8ff4b4
Awarix <just_tevy@mail.ru>
2025-05-16 22:03:20 +0300

    fix linter2

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
         Join collaborative hunts on Monad Testnet. Make moves, follow hints, and mint your map NFT.
       </p>
       <p className="text-lg md:text-xl text-[var(--theme-text-primary)] mb-10 text-center max-w-xl">
-        I'm still working on this. But I appreciate that you're here, my dear alfa tester :P
+        I'm still working on this. But I appreciate that you're here, my dear alfa tester!
       </p>
       <p className="text-lg md:text-xl text-[var(--theme-text-primary)] mb-10 text-center max-w-xl">
         If you have any feedback, please dm me @frontend. 
